cb8662dd4fa10458a56c2a1ef1043383ea660e9a
Eduardo Faria <z.eduardofaria@gmail.com>
2020-05-18 16:24:15 -0300

    Dashboard styled

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,61 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore Github repositories</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore Github repositories</Title>
+
+      <Form action="">
+        <input placeholder="Type the repository name" />
+        <button type="submit">Search</button>
+      </Form>
+
+      <Repositories>
+        <a href="tst">
+          <img
+            src="https://avatars0.githubusercontent.com/u/23220615?s=460&u=41eb573e1b02ef6fc1f4d08996bf0d16596c9563&v=4"
+            alt="Eduardo Faria"
+          />
+          <div>
+            <strong>zEduardofaria/gobarber-api-ts</strong>
+            <p>Easy peasy lemon squeezy</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="tst">
+          <img
+            src="https://avatars0.githubusercontent.com/u/23220615?s=460&u=41eb573e1b02ef6fc1f4d08996bf0d16596c9563&v=4"
+            alt="Eduardo Faria"
+          />
+          <div>
+            <strong>zEduardofaria/gobarber-api-ts</strong>
+            <p>Easy peasy lemon squeezy</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="tst">
+          <img
+            src="https://avatars0.githubusercontent.com/u/23220615?s=460&u=41eb573e1b02ef6fc1f4d08996bf0d16596c9563&v=4"
+            alt="Eduardo Faria"
+          />
+          <div>
+            <strong>zEduardofaria/gobarber-api-ts</strong>
+            <p>Easy peasy lemon squeezy</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
